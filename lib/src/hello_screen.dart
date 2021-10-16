4a1c5fec8b75e5fc84f7e356ceac830826a2c333
Thrauk <stoicath@gmail.com>
2021-10-16 22:19:20 +0300

    lab2 using flutter

--- lib/src/hello_screen.dart
+++ lib/src/hello_screen.dart
@@ -1,12 +1,10 @@
 import 'dart:core';
-import 'dart:core';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'dart:math';
-
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share/share.dart';
 void main() {
   runApp(MyApp());
 }
@@ -28,6 +26,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String greetingText = 'Greetings';
   String formText = '';
+  List<String> colorChoices = ['White', 'Red', 'Black', 'Orange'];
+  List<Color> colorList = [Colors.white, Colors.red, Colors.black, Colors.orange];
+  String activeChoice = 'White';
+  Color buttonColor = Colors.white;
+
+  void _launchURL(url) async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
 
   @override
   Widget build(BuildContext context) {
@@ -65,15 +70,24 @@ class _HomePageState extends State<HomePage> {
                     child: ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          if(formText != '') {
+                          if (formText != '') {
                             greetingText = 'Hello, $formText';
-                          }
-                          else {
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  PopupWindowWidget(
+                                greetingText: greetingText,
+                              ),
+                            );
+                          } else {
                             greetingText = 'Greetings';
                           }
                         });
                       },
-                      child: const Text('SAY HELLO'),
+                      child: Text(
+                        'SAY HELLO',
+                        style: TextStyle(color: buttonColor),
+                      ),
                     ),
                   ),
                 ],
@@ -86,12 +100,110 @@ class _HomePageState extends State<HomePage> {
               greetingText,
               style: TextStyle(
                 fontSize: 20,
-                color: greetingText == 'Greetings' ? Colors.grey : Colors.blueAccent,
+                color: greetingText == 'Greetings'
+                    ? Colors.grey
+                    : Colors.blueAccent,
+              ),
+            ),
+            Row(
+              children: [
+                const Expanded(child: Text('Choose button color:')),
+                Expanded(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    value: activeChoice,
+                    onChanged: (String? newValue) {
+                      if(newValue != null)
+                      {
+                        int index = colorChoices.indexOf(newValue);
+                        setState(() {
+                          activeChoice = newValue;
+                          buttonColor = colorList[index];
+                        });
+                      }
+
+                    },
+                    items: colorChoices
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if(formText != '') {
+                  setState(() {
+                    Share.share(formText);
+                  });
+                }
+              },
+              child: Text(
+                'Share',
+                style: TextStyle(color: buttonColor),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if(formText != '') {
+                  String formattedName = formText.replaceAll(' ', '+');
+                  String url = 'https://www.google.com/search?q=$formattedName';
+                  setState(() {
+                    _launchURL(url);
+                  });
+                }
+              },
+              child: Text(
+                'Search',
+                style: TextStyle(color: buttonColor),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class PopupWindowWidget extends StatelessWidget {
+  const PopupWindowWidget({Key? key, required this.greetingText})
+      : super(key: key);
+
+  final String greetingText;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Greeting'),
+      content: Text(greetingText),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context, 'NEGATIVE');
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(const SnackBar(
+                content: Text("NEGATIVE"),
+              ));
+          },
+          child: const Text('NEGATIVE'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context, 'POSITIVE');
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(const SnackBar(
+                content: Text("POSITIVE"),
+              ));
+          },
+          child: const Text('POSITIVE'),
+        ),
+      ],
     );
   }
 }
